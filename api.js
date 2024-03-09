2e881bc24a96563638eb165fc8afae42ccb25a7d
ghadifrangieh <ghadi.frangieh@hotmail.com>
2024-03-09 12:11:06 +0200

    adding a new endpoint students

--- api.js
+++ api.js
@@ -8,5 +8,8 @@ app.use(cors())
 app.get('/', function (req , res) {
   res.send('Hello World')
 })
-
+app.get('/students', function (req , res) {
+    let students=[{id:20,name:'ghadi'},{id:12,name:'emile'}];
+    res.send(students)
+  })
 app.listen(3000)
